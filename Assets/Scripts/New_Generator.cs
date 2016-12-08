43aa172c14b9569e6df59092328c915eba2e374a
alexr97 <alex.r.97@mail.ru>
2016-12-08 18:15:38 +0300

    Finally added level generator

---
+++ Assets/Scripts/New_Generator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class New_Generator
+    {
+        public string StringMatrixGenerator(int n)
+        {
+            Random r = new Random();
+            StringBuilder sb = new StringBuilder(n * n);
+
+            for (int i = 0; i < n * n; i++)
+            {
+                sb.Append(r.Next(0, 10));
+            }
+
+            var s = sb.ToString();
+
+            return s;
+        }
+    }
+}
