326e63e5c4ffe15ec5872047b2850365c98e8cd1
alexr97 <alex.r.97@mail.ru>
2016-12-18 20:24:12 +0300

    DB logic upgrade

---
+++ Assets/Scripts/DBEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class DBEditor
+    {
+        public void Filler()
+        {
+
+        }
+
+        public void Getter()
+        {
+
+        }
+    }
+}
